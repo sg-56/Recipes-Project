7bb47e944d9fbbd6692567731c5ab6d7727b3648
sg-56 <shivashankar944@gmail.com>
2024-04-04 12:56:08 +0530

    Created a new Branch- UI for rock-paper-scissor

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,39 @@
-function alert_box(){
-    window.alert("Error occured!!")
+const list  = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-    return `<h2> This is a new text</h2>`
-}
+button.addEventListener('click',() => {
+
+    const Myitem = input.value;
+
+    if (Myitem === ''){
+        alert("Enter a value to add to the list");
+    }
+    else{
+    input.value = '';
+
+    
+    console.log(Myitem);
+    const listitem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listButton = document.createElement("button");
+
+    listitem.appendChild(listText);
+    listText.textContent = Myitem;
+    listitem.appendChild(listButton);
+    listButton.textContent = 'Delete';
+    list.appendChild(listitem);
+
+
+    listButton.addEventListener("click",() =>{
+        list.removeChild(listitem);
+    });
+
+    input.focus();
+};
+
+});
+
+
+
+  
